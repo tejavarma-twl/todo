1fe5ae6f84865d0fc4d98f54a5257b6c6e4180f4
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2017-09-17 23:42:37 +0530

    Added database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,19 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{
+
+let env = 'prod';
+
+if(env==='prod'){
+    dburl = 'mongodb://tej:tej@ds141284.mlab.com:41284/todoapp';
+}else{
+    dburl = 'mongodb://localhost:27017/todoapp';
+}
+
+mongoose.connect(dburl,{
     useMongoClient:true
 });
 
 module.exports = {mongoose};
+
+
